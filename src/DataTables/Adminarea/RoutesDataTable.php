4ff77af78d38e5035639e6875dd5511114483d2c
Abdelrahman Omran <me@omranic.com>
2017-11-22 16:18:20 +0200

    Add datatable limit menu to button strip

--- src/DataTables/Adminarea/RoutesDataTable.php
+++ src/DataTables/Adminarea/RoutesDataTable.php
@@ -75,6 +75,7 @@ class RoutesDataTable extends AbstractDataTable
             'buttons' => [
                 'print', 'reset', 'reload', 'export',
                 ['extend' => 'colvis', 'text' => '<i class="fa fa-columns"></i> '.trans('cortex/foundation::common.columns').' <span class="caret"/>'],
+                ['extend' => 'pageLength', 'text' => '<i class="fa fa-list-ol"></i> '.trans('cortex/foundation::common.limit').' <span class="caret"/>'],
             ],
         ];
     }
