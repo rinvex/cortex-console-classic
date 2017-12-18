61ea9e020393bd1c9c0f4c4d79a5f944602f10df
Abdelrahman Omran <me@omranic.com>
2017-12-18 16:47:52 +0000

    Apply fixes from StyleCI

--- routes/menus.php
+++ routes/menus.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
-Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+Menu::modify('adminarea.sidebar', function (MenuFactory $menu) {
     $menu->findBy('title', trans('cortex/foundation::common.maintenance'), function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.console.routes.index'], trans('cortex/console::common.routes'), 10, 'fa fa-globe')->can('list-routes');
         $dropdown->route(['adminarea.console.terminal.index'], trans('cortex/console::common.terminal'), 20, 'fa fa-terminal')->can('run-terminal');
