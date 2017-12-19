f03e9ad942389bf3636e3d62828a74296d3db624
Abdelrahman Omran <me@omranic.com>
2017-12-19 17:38:12 +0200

    Tweak menus

--- routes/menus.php
+++ routes/menus.php
@@ -6,7 +6,7 @@ use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
 Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
-    $menu->findBy('title', trans('cortex/foundation::common.maintenance'), function (MenuItem $dropdown) {
+    $menu->findByTitleOrAdd(trans('cortex/foundation::common.maintenance'), 999, 'fa fa-cogs', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.console.routes.index'], trans('cortex/console::common.routes'), 10, 'fa fa-globe')->can('list-routes');
         $dropdown->route(['adminarea.console.terminal.index'], trans('cortex/console::common.terminal'), 20, 'fa fa-terminal')->can('run-terminal');
     });
