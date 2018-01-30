aca52cab03774a9b716f0993fb2d08ea79e9d8e3
Abdelrahman Omran <me@omranic.com>
2018-01-31 01:48:59 +0200

    Replace HTML comments with Blade comments

--- resources/views/adminarea/pages/terminal.blade.php
+++ resources/views/adminarea/pages/terminal.blade.php
@@ -40,31 +40,34 @@
             <h1>{{ Breadcrumbs::render() }}</h1>
         </section>
 
-        <!-- Main content -->
+        {{-- Main content --}}
         <section class="content">
+
             <div class="row">
+
                 <div class="col-md-12">
+
                     <div class="box">
+
                         <div class="box-header">
                             <h3 class="box-title">
                                 {{ trans('cortex/console::common.terminal') }}
                             </h3>
                         </div>
-                        <!-- /.box-header -->
+
                         <div class="box-body">
-
                             <div id="terminal-shell"></div>
-
                         </div>
-                        <!-- /.box-body -->
+
                     </div>
-                    <!-- /.box -->
+
                 </div>
-                <!-- /.col -->
+
             </div>
-            <!-- /.row -->
+
         </section>
-        <!-- /.content -->
+
     </div>
+
 
 @endsection
