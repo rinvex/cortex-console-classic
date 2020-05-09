ba77c6177472fb3b29a2d068bde1df1bfe917013
Abdelrahman Omran <me@omranic.com>
2020-05-10 00:53:59 +0400

    Fix compatibility with recent rinvex/laravel-menus package update

--- routes/menus/adminarea.php
+++ routes/menus/adminarea.php
@@ -6,7 +6,7 @@ use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Models\MenuGenerator;
 
 Menu::register('adminarea.sidebar', function (MenuGenerator $menu) {
-    $menu->findByTitleOrAdd(trans('cortex/foundation::common.maintenance'), 999, 'fa fa-cogs', [], function (MenuItem $dropdown) {
+    $menu->findByTitleOrAdd(trans('cortex/foundation::common.maintenance'), 999, 'fa fa-cogs', 'header', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.console.routes.index'], trans('cortex/console::common.routes'), 10, 'fa fa-globe')->ifCan('list-routes');
         $dropdown->route(['adminarea.console.terminal.index'], trans('cortex/console::common.terminal'), 20, 'fa fa-terminal')->ifCan('run-terminal');
     });
