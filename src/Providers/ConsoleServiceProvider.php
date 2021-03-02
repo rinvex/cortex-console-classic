cff7972c48b3076098d704f4313eb560f79b284f
Abdelrahman Omran <me@omranic.com>
2021-03-02 07:21:17 +0400

    Autoload artisan commands

--- src/Providers/ConsoleServiceProvider.php
+++ src/Providers/ConsoleServiceProvider.php
@@ -12,25 +12,11 @@ use Cortex\Console\Console\Commands\Tail;
 use Cortex\Console\Console\Commands\Mysql;
 use Cortex\Console\Console\Commands\Artisan;
 use Cortex\Console\Console\Commands\Composer;
-use Cortex\Console\Console\Commands\SeedCommand;
 use Cortex\Console\Console\Commands\ArtisanTinker;
-use Cortex\Console\Console\Commands\InstallCommand;
-use Cortex\Console\Console\Commands\PublishCommand;
 
 class ConsoleServiceProvider extends ServiceProvider
 {
     use ConsoleTools;
-
-    /**
-     * The commands to be registered.
-     *
-     * @var array
-     */
-    protected $commands = [
-        SeedCommand::class => 'command.cortex.console.seed',
-        PublishCommand::class => 'command.cortex.console.publish',
-        InstallCommand::class => 'command.cortex.console.install',
-    ];
 
     /**
      * Register any application services.
@@ -43,9 +29,6 @@ class ConsoleServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Register console commands
-        $this->registerCommands($this->commands);
-
         $this->app->singleton(Terminal::class, function ($app) {
             $_SERVER['PHP_SELF'] = 'artisan'; // Fix (index.php => artisan)
 
