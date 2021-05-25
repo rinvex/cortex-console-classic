df16391e3c8f684b027e01b1fb2a621d9507be3e
Abdelrahman Omran <me@omranic.com>
2021-05-25 17:56:47 +0400

    Replace deprecated `Breadcrumbs::register` with `Breadcrumbs::for`

--- routes/breadcrumbs/adminarea.php
+++ routes/breadcrumbs/adminarea.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('adminarea.cortex.console.terminal.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.console.terminal.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
     $breadcrumbs->push(trans('cortex/console::common.terminal'), route('adminarea.cortex.console.terminal.index'));
 });
 
-Breadcrumbs::register('adminarea.cortex.console.routes.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.console.routes.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
     $breadcrumbs->push(trans('cortex/console::common.routes'), route('adminarea.cortex.console.routes.index'));
 });
